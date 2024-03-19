c72bb309858f687ec8672f4c29a116c116fa00c1
priscilakovalek <126891120+priscilakovalek@users.noreply.github.com>
2024-03-19 11:10:45 -0300

    metas adicionadas

--- main.js
+++ main.js
@@ -2,6 +2,8 @@ const botoes=document.querySelectorAll(".botao")
 console.log (botoes);
 for (let i=0;i<botoes.length;i++){
     botoes[i].onclick=function(){
-        
+        for(let j=0);j>botoes.length;j++){
+        botões[j].classList.classList.remove("ativo");
     }
+      botoes[i]this.classList.add("ativo");
 }

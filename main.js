13d971b9ccfb5ea3b59e1fe3ba6953acbce5d616
priscilakovalek <126891120+priscilakovalek@users.noreply.github.com>
2024-04-08 11:12:06 -0300

    parte 5 concluida

--- main.js
+++ main.js
@@ -1,9 +1,21 @@
 const botoes=document.querySelectorAll(".botao")
-console.log (botoes);
+ const textos = document.querySelectorAll(".aba-conteudo");
+
 for (let i=0;i<botoes.length;i++){
     botoes[i].onclick=function(){
-        for(let j=0);j>botoes.length;j++){
+
+        for(let j=0;j>botoes.length;j++){
         botões[j].classList.classList.remove("ativo");
+        botões[j].classList.classList.remove("ativo"); 
     }
-      botoes[i]this.classList.add("ativo");
+
+      botoes[i].classList.add("ativo");
+      textos[i].classList.add("ativo");
 }
+}
+
+const contadores=document.querySelectorAll(".contador");
+const tempoObjetivo1 =new Date("2024-10-05T00:00:00");
+let temṕoAtual= new Date();
+
+contadores[0].textContent=tempoObjetivo1- temṕoAtual; 

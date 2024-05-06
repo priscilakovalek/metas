48077004e739cae96e07eb5f85f0fb3be797c676
priscilakovalek <126891120+priscilakovalek@users.noreply.github.com>
2024-05-06 09:46:51 -0300

    projeto fanalizado

--- main.js
+++ main.js
@@ -14,13 +14,13 @@ for (let i = 0; i < botoes.length; i++) {
     }
 }
 
-const contadores=document.querySelectorAll(".contador");
+const contadores = document.querySelectorAll(".contador");
 const tempoObjetivo1 = new Date("2023-10-05T00:00:00");
-const tempoObjetivo2= new Date("2023-12-05T00:00:00");
-const tempoObjetivo3= new Date("2023-12-30T00:00:00");
-const tempoObjetivo4= new Date("2024-12-01T00:00:00");
+const tempoObjetivo2 = new Date("2023-12-05T00:00:00");
+const tempoObjetivo3 = new Date("2023-12-30T00:00:00");
+const tempoObjetivo4 = new Date("2024-02-01T00:00:00");
 
-contadores[0].textContent = calculaTempo(tempoObjetivo1);
+const tempos = [tempoObjetivo1,tempoObjetivo2,tempoObjetivo3,tempoObjetivo4];
 
 
 function calculaTempo(tempoObjetivo) {
@@ -34,23 +34,25 @@ function calculaTempo(tempoObjetivo) {
     segundos %= 60;
     minutos %= 60;
     horas %= 24;
-    if(tempoFinal >0){
-        return dias + " dias " + horas + " horas " + minutos + " minutos " + segundos + " segundos";
-    }else{
-        return"Prazo Finalizado";
-      }
+    if (tempoFinal > 0){
+        return [dias,horas,minutos,segundos];
+    } else {
+        return [0,0,0,0];
     }
+}
 
-    function atualizaCronometro(){
-        
-        for (let i=0; i<contadores.length;i++){
-            contadores[i].textContent = calculaTempo(tempos[i]);
-        }
+function atualizaCronometro(){
+    for (let i=0; i<contadores.length;i++){
+        document.getElementById("dias"+i).textContent = calculaTempo(tempos[i])[0];
+        document.getElementById("horas"+i).textContent = calculaTempo(tempos[i])[1];
+        document.getElementById("min"+i).textContent = calculaTempo(tempos[i])[2];
+        document.getElementById("seg"+i).textContent = calculaTempo(tempos[i])[3];   
     }
+}
 
-    function comecaCronometro(){
-        atualizaCronometro();
-        setInterval(atualizaCronometro,1000);
-    }
+function comecaCronometro(){
+    atualizaCronometro();
+    setInterval(atualizaCronometro,1000);
+}
 
-//comecaCronometro();
+comecaCronometro();
